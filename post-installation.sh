84ffdb6832d36769c9571e4edba16abd549c8d83
Adam Popp <adampopp@adamwritescode.com>
2025-05-04 15:15:55 -0400

    Work in progress fixed issue with how I was moving the config files to the config destinations.

--- post-installation.sh
+++ post-installation.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
 #Variables
+CONFIGS_SOURCE=Configs
 CONFIG_DIR=$XDG_CONFIG_HOME
+INSTALL_PACKAGES="paru -S --noconfirm"
+
+#Functions
+CopyFileToDirectory()
+{
+    echo "Copying" $1 "To the" $2 "directory"
+    mkdir -p $2
+    cp $1 $2
+}
 
 #Update the make package configuration to allow up to 6 threads to be used to when building packages.
-cat Configs/makepkg.conf >> /etc/makepkg.conf
-mkdir $CONFIG_DIR/alacritty
-touch $CONFIG_DIR/alacritty/alacritty.toml
-cat Configs/alacritty.toml >> $CONFIG_DIR/alacritty/alacritty.toml
+CopyFileToDirectory $CONFIGS_SOURCE/makepkg.conf /etc/
+CopyFileToDirectory $CONFIGS_SOURCE/alacritty.toml $CONFIG_DIR/alacritty/
 
 #install AUR package manager (paru)
 pacman -S --noconfirm git base-devel
@@ -19,4 +27,4 @@ cd ../
 rm -rf paru
 
 #install
-paru -S --noconfirm brave-bin alacritty vscodium-bin spotify discord, curl i3-wm
+$INSTALL_PACKAGES brave-bin alacritty vscodium-bin spotify discord, curl i3-wm
